d8406066cb4def435138d37a549dce81dd0c5816
Przemek Luty <przemyslawluty92@gmail.com>
2022-02-12 14:37:06 +0100

    new file: przemek.html

--- webpack.common.js
+++ webpack.common.js
@@ -88,5 +88,11 @@ module.exports = {
       chunks: ["index", "another"],
       filename: "another.html",
     }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/another.html",
+      inject: true,
+      chunks: ["index",],
+      filename: "przemek.html",
+    }),
   ],
 };
